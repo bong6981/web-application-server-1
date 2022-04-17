2f229cc87f4187a09f2d3cbbd7b6c0b935b71226
bong6981 <kbk5190@naver.com>
2022-04-18 00:09:18 +0900

    요구사항1 - index.html - utils 분리(강의)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import util.HttpRequestUtils;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -32,19 +34,10 @@ public class RequestHandler extends Thread {
                 return;
             }
 
-            String[] splited = line.split(" ");
-            String path = splited[1];
-            log.debug("request path : {}", path);
-
-            while (!line.equals("")) {
-                line = br.readLine();
-                log.debug("header : {}", line);
-            }
-
-            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+            String url = HttpRequestUtils.getUrl(line);
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 
             DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
